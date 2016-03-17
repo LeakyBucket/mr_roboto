6d032049029a164404e09fea3e1c2083f42d3f0d
Glen Holcomb <DamnBigMan@gmail.com>
2016-03-16 19:31:26 -0600

    Adds `Agent` for fetching `robots.txt` from site

---
+++ lib/mr_roboto/agent.ex
@@ -0,0 +1,50 @@
+defmodule MrRoboto.Agent do
+  use GenServer
+
+  @default_expiration 21600
+
+  def start_link do
+    HTTPoison.start
+
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  def handle_call({:check, site}, state) do
+    case update_robot(site, state) do
+      {:ok, decision, new_history} ->
+        {:reply, {:ok, decision}, new_history}
+      {:error, error, state} ->
+        {:reply, {:error, error}, state}
+    end
+  end
+
+  def default_expiration do
+    @default_expiration
+  end
+
+  def update_robot(site, history) do
+    case Map.fetch(history, site) do
+      {:ok, fetched_at} ->
+        if (fetched_at + @default_expiration) > :erlang.system_time(:seconds) do
+          {:ok, :current, history}
+        else
+          fetch_robots(site, history)
+        end
+      :error ->
+        fetch_robots(site, history)
+    end
+  end
+
+  def fetch_robots(site, history) do
+    case HTTPoison.get(site <> "/robots.txt") do
+      {:ok, response} ->
+        {:ok, response.body, update_timestamp(site, history)}
+      {:error, reason} ->
+        {:error, reason, history}
+    end
+  end
+
+  def update_timestamp(site, history) do
+    Map.put(history, site, :erlang.system_time(:seconds))
+  end
+end
---
+++ test/mr_roboto/agent_test.exs
@@ -0,0 +1,45 @@
+defmodule MrRoboto.AgentTest do
+  use ExUnit.Case, async: true
+
+  alias MrRoboto.Agent
+
+  @checked "https://www.google.com"
+  @history %{@checked => :erlang.system_time(:seconds)}
+
+  test "update_robot indicates if the robots file has been fetched and is current" do
+    assert {:ok, :current, _} = Agent.update_robot(@checked, @history)
+  end
+
+  test "update_robot fetches the robots.txt file if it has never been retrieved" do
+    {:ok, robots_body, _} = Agent.update_robot("https://www.lawlytics.com", @history)
+
+    assert is_binary(robots_body)
+  end
+
+  test "update_robot fetches the robots.txt file if the last check is too old" do
+    long_ago = :erlang.system_time(:seconds) - (Agent.default_expiration + 1)
+    old_check = %{@checked => long_ago}
+
+    {:ok, robots_body, _} = Agent.update_robot(@checked, old_check)
+
+    assert is_binary(robots_body)
+  end
+
+  test "fetch_robots retrieves the robots.txt file for the given site" do
+    assert {:ok, response, _} = Agent.fetch_robots("https://www.lawlytics.com", @history)
+    assert is_binary(response)
+  end
+
+  test "update_timestamp updates the check time for the site" do
+    the_past = :erlang.system_time(:seconds) - 30
+    %{"https://www.google.com" => nowish} = Agent.update_timestamp(@checked, %{"https://www.google.com" => the_past})
+
+    assert nowish > the_past
+  end
+
+  test "update_timestamp enters a timestamp if there isn't one already" do
+    new_history = Agent.update_timestamp("https://www.lawlytics.com", @history)
+
+    assert Map.get(new_history, "https://www.lawlytics.com")
+  end
+end
