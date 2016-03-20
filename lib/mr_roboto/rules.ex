81c5ca0b4a96985d9d23b334dc412f8146e98932
Glen Holcomb <DamnBigMan@gmail.com>
2016-03-20 09:14:25 -0600

    Reworking `Rules` module to actually enforce rules.

--- lib/mr_roboto/rules.ex
+++ lib/mr_roboto/rules.ex
@@ -1,19 +1,60 @@
 defmodule MrRoboto.Rules do
+  @moduledoc """
+  The Rules module defines the rules for a user-agent.
+
+  The Rules module defines the MrRoboto.Rules struct and a few functions which allow querying the rule set along with one function for setting the `crawl_delay`.
+  """
+
   defstruct user_agent: "", allow: [], disallow: [], crawl_delay: 1000
 
-  def agent(rule, name) do
-    struct(rule, user_agent: name)
-  end
+  @doc """
+  Sets the *Crawl Delay* for the `user-agent`
 
-  def allow(rule, path) do
-    struct(rule, allow: [path] ++ rule.allow)
-  end
+  Returns an updated `MrRoboto.Rules` struct
 
-  def disallow(rule, path) do
-    struct(rule, disallow: [path] ++ rule.disallow)
-  end
+  ## Examples
 
+    iex> rule = %MrRoboto.Rules{user_agent: "*", allow: ["/"], disallow: [], crawl_delay: 1000}
+    ...> MrRoboto.Rules.set_delay(rule, 2000)
+    %MrRoboto.Rules{user_agent: "*", allow: ["/"], disallow: [], crawl_delay: 2000}
+
+  """
   def set_delay(rule, frequency) do
     struct(rule, crawl_delay: frequency)
+  end
+
+  @doc """
+  Determines whether the specified `path` is allowed by the given `rule`
+
+  Returns `true` or `false`
+  """
+  def allowed?(rule, path) do # Collect allow and disallow matches, the type with the longest match wins
+
+  end
+
+  @doc """
+  Determines the direction to walk the path for the given directive
+
+  Returns `:forwards` or `:backwards`
+
+  ## Examples
+
+  ```
+  iex> MrRoboto.Rules.match_direction "/*.php$"
+  :backwards
+  ```
+  ```
+  iex> MrRoboto.Rules.match_direction "/foo/"
+  :forwards
+  ```
+
+  """
+  def match_direction(directive) do
+    case :binary.last(directive) do
+      ?$ ->
+        :backwards
+      _ ->
+        :forwards
+    end
   end
 end
--- test/mr_roboto/rules_test.exs
+++ test/mr_roboto/rules_test.exs
@@ -3,27 +3,18 @@ defmodule MrRoboto.RulesTest do
 
   alias MrRoboto.Rules
 
-  @agent "MrRoboto"
-  @root "/"
-  @first_level "/first/"
-  @second_level "/first/second/"
-  @delay 2000
-
   @rules %Rules{}
-
-  test "allow adds a path to the allowed list" do
-    assert [@second_level] = Rules.allow(@rules, @second_level).allow
-  end
-
-  test "disallow adds a path to the disallowed list" do
-    assert [@first_level] = Rules.disallow(@rules, @first_level).disallow
-  end
-
-  test "agent sets the user_agent for the rule set" do
-    assert @agent = Rules.agent(@rules, @agent).user_agent
-  end
+  @delay 2000
 
   test "set_delay sets the crawl_delay for the rule set" do
     assert @delay = Rules.set_delay(@rules, @delay).crawl_delay
+  end
+
+  test "it indicates a forward match direction if the directive doesn't end in '$'" do
+    assert :forwards = Rules.match_direction "/foo/"
+  end
+
+  test "it indicates a backwards match direction if the directive ends in '$'" do
+    assert :backwards = Rules.match_direction "/*.php$"
   end
 end
