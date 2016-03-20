7c005fb3c648199606360dd96f7c25694d2ae4dc
Glen Holcomb <DamnBigMan@gmail.com>
2016-03-20 12:35:36 -0600

    Adds directive matching and direction detection.

--- lib/mr_roboto/rules.ex
+++ lib/mr_roboto/rules.ex
@@ -57,4 +57,43 @@ defmodule MrRoboto.Rules do
         :forwards
     end
   end
+  def directive_applies?("", _remaining_path), do: true
+  def directive_applies?(_remaining_directive, ""), do: false
+  def directive_applies?(<<d :: size(8), d_rest :: binary>>, <<p :: size(8), p_rest :: binary>>) do
+    case d do
+      ?* ->
+        case handle_wildcard(d_rest, p_rest) do
+          {:ok, directive, path} ->
+            directive_applies? directive, path
+          {:error, :exhausted} ->
+            false
+        end
+      ^p ->
+        directive_applies? d_rest, p_rest
+      _ ->
+        false
+    end
+  end
+
+  defp handle_wildcard("", _remaining_path), do: {:ok, "", ""}
+  defp handle_wildcard(<<target :: size(8), remaining_directive :: binary>>, path) do
+    case consume_until(path, target) do
+      {:ok, remaining_path} ->
+        {:ok, remaining_directive, remaining_path}
+      _ ->
+        {:error, :exhausted}
+    end
+  end
+
+  defp consume_until("", _target), do: {:error, :exhausted}
+  defp consume_until(<<char :: size(8), rest:: binary>>, target) do
+    case char do
+      ^target ->
+        {:ok, rest}
+      _ ->
+        consume_until rest, target
+    end
+  end
+
+  defp reverse(string), do: List.to_string(Enum.reverse to_char_list(string))
 end
--- test/mr_roboto/rules_test.exs
+++ test/mr_roboto/rules_test.exs
@@ -17,4 +17,20 @@ defmodule MrRoboto.RulesTest do
   test "it indicates a backwards match direction if the directive ends in '$'" do
     assert :backwards = Rules.match_direction "/*.php$"
   end
+
+  test "it correctly indicates if a regular directive applies" do
+    assert Rules.directive_applies? "/foo", "/foo/bar/page"
+  end
+
+  test "it correctly indicates if a wildcarded directive applies" do
+    assert Rules.directive_applies? "/foo*s", "/foo/bar/pages"
+  end
+
+  test "it correctly indicates if a regular directive does not apply" do
+    refute Rules.directive_applies? "/foo/", "/foo"
+  end
+
+  test "it correctly indicates if a wildcarded directive does not apply" do
+    refute Rules.directive_applies? "/f*s", "/fido/"
+  end
 end
