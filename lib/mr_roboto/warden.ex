d13fc8e80203568b31b23a5963d65342b07cfe20
Glen Holcomb <DamnBigMan@gmail.com>
2016-03-27 12:35:43 -0600

    Adds initial `Warden`

---
+++ lib/mr_roboto/warden.ex
@@ -0,0 +1,162 @@
+defmodule MrRoboto.Warden do
+  @moduledoc """
+  The Warden is responsible for coordinating the retrieval, parsing and checking
+  of `robots.txt` files.
+
+  It maintains a map of hosts checked and the rules for those hosts.  When asked
+  it will retrieve the rule in question and check to see if the specified
+  `user-agent` is allowed to crawl that path.
+  """
+
+  use GenServer
+
+  alias MrRoboto.Agent
+  alias MrRoboto.Parser
+  alias MrRoboto.Rules
+
+  defstruct rule: nil, last_check: 0
+
+  def handle_call({:crawl?, {agent, url}}, _from, state) do
+    uri = URI.parse(url)
+    updated_records = update_records(uri.host, state)
+
+    agent
+    |> fetch_record(uri.host, updated_records)
+    |> permitted?(uri.path)
+    |> case do
+      true ->
+        {:reply, :allowed, updated_records}
+      false ->
+        {:reply, :disallowed, updated_records}
+      :ambiguous ->
+        {:reply, :ambiguous, updated_records}
+    end
+  end
+
+  def handle_cast({:set_check, url, agent}, state) do
+    new_state = update_in state, [URI.parse(url).host, agent, :last_check], :erlang.system_time(:seconds)
+
+    {:noreply, new_state}
+  end
+
+  @doc """
+  Retrieves the record for the user-agent/host pair provided
+
+  Returns a `%MrRoboto.Warden` struct or `nil`
+
+  In the event that a record is not found for the specific _user-agent_ then the
+  wildcard record is returned.
+  """
+  def fetch_record(user_agent, host, records) do
+    get_in(records, [host, user_agent])
+    |> case do
+      %__MODULE__{} = found ->
+        found
+      _ ->
+        get_in records, [host, "*"]
+    end
+  end
+
+  @doc """
+  Updates the state for the Warden.
+
+  Returns a nested map of Warden structs.
+
+  If the `robots.txt` data is stale or it has not been fetched yet it returns a
+  new nested map of Warden structs.  In the event of an `:error` or the data
+  being current it returns the current records map.
+
+  ## Examples
+
+    _update_records_ is responsible for managing the state for the Warden server.
+    This state is a collection of `MrRoboto.Warden.t` structs indexed by host
+    and user-agent, where there is __one__ `MrRoboto.Warden.t` struct per
+    _user-agent_ and potentially many _user-agents_ per _host_.
+
+    ```
+      %{
+        "google.com" => %{
+          "*" => %{rule: %MrRoboto.Rules{}, last_check: time_in_seconds},
+        }
+      }
+    ```
+
+  """
+  def update_records(host, records) do
+    GenServer.call(Agent, {:check, host})
+    |> case do
+      {:ok, :current} ->
+        records
+      {:ok, body} ->
+        body
+        |> Parser.start_parse
+        |> insert(records, host)
+      {:error, _reason} ->
+        records
+    end
+  end
+
+  defp insert(rule_set, current_records, host) do
+    rules = Enum.into(rule_set, %{}, fn rule ->
+      {rule.user_agent, rule}
+    end)
+
+    Map.put current_records, host, rules
+  end
+
+  @doc """
+  Indicates whether the given path can be crawled
+
+  Retrns `true`, `false` or `:ambiguous`
+
+  The `Warden` takes two factors into consideration when determining whether a
+  path is legal.  The first factor are the directives in `robots.txt` for the
+  user-agent.  The second is the last time a check was recorded for the site.
+
+  ## Examples
+
+    If the rule indicates that the path is legal then the `last_check` time is
+    compared to the current time before returning an answer.
+
+    ```
+    iex> Warden.permitted? %Warden{rule: %Rules{user_agent: "*", allow: ["/"], disallow: [], crawl_delay: 1000}, last_check: 0}, "/"
+    # assuming the current time is large enough
+    true
+    # assuming the current time is say 5
+    false
+    ```
+
+    In the case where the `MrRoboto.Rules` struct indicates that the rule is
+    not permitted then `false` is returned.
+
+    __Note__: The `last_check` is not considered in this case
+
+    ```
+    iex> Warden.permitted? %Warden{rule: %Rules{user_agent: "*", allow: [], disallow: ["/"]}}, "/"
+    false
+    ```
+
+    In the case where the `MrRoboto.Rules` struct is ambiguous as to permission
+    then `:ambiguous` is returned.
+
+    __Note__: The `last_check` is not considered in this case
+
+    ```
+    iex> Warden.permitted? %Warden{rule: %Rules{user_agent: "*", allow: ["/"], disallow: ["/"]}}, "/"
+    ```
+
+  """
+  def permitted?(%__MODULE__{rule: rule, last_check: last_check}, path) do
+    ok_after = last_check + rule.crawl_delay
+
+    Rules.permitted?(rule, path)
+    |> case do
+      true ->
+        ok_after < :erlang.system_time(:seconds)
+      false ->
+        false
+      :ambiguous ->
+        :ambiguous
+    end
+  end
+end
---
+++ test/mr_roboto/warden_test.exs
@@ -0,0 +1,69 @@
+defmodule MrRoboto.WardenTest do
+  use ExUnit.Case, async: true
+
+  alias MrRoboto.Agent
+  alias MrRoboto.Rules
+  alias MrRoboto.Warden
+
+  @records %{
+    "www.google.com" => %{
+      "*" => %Warden{rule: %Rules{user_agent: "*", allow: ["/foo"], disallow: ["/bar"]}, last_check: nil}
+    },
+    "www.lawlytics.com" => %{
+      "*" => %Warden{rule: %Rules{user_agent: "*", allow: ["/foo"], disallow: ["/bar"]}, last_check: nil},
+      "google" => %Warden{rule: %Rules{user_agent: "google", allow: ["/foo"], disallow: ["/bar"]}, last_check: nil}
+    }
+  }
+
+  test "it fetches the Warden struct for the given host and agent" do
+    assert %Warden{rule: %Rules{user_agent: "google"}} = Warden.fetch_record "google", "www.lawlytics.com", @records
+  end
+
+  test "it returns the '*' rule if there is no specific agent record" do
+    assert %Warden{rule: %Rules{user_agent: "*"}} = Warden.fetch_record "google", "www.google.com", @records
+  end
+
+  test "it doesn't update the records if the 'robots.txt' is current" do
+    GenServer.call Agent, {:check, "www.yahoo.com"}
+
+    assert @records = Warden.update_records "www.yahoo.com", @records
+  end
+
+  test "it updates the records if the 'robots.txt' file is not current" do
+    updated = Warden.update_records "www.bing.com", @records
+
+    assert Map.has_key? updated, "www.bing.com"
+  end
+
+  test "it doesn't update the records if there was an error" do
+    assert @records = Warden.update_records "uncle", @records
+  end
+
+  test "it allows a check if the rule allows and the last check is sufficiently old" do
+    rule = %Rules{user_agent: "*", allow: ["/"], disallow: []}
+    warden_record = %Warden{rule: rule, last_check: :erlang.system_time(:seconds) - 3000}
+
+    assert Warden.permitted?(warden_record, "/")
+  end
+
+  test "it disallows a check if the rule allows but the last check was too recent" do
+    rule = %Rules{user_agent: "*", allow: ["/"], disallow: []}
+    warden_record = %Warden{rule: rule, last_check: :erlang.system_time(:seconds)}
+
+    refute Warden.permitted?(warden_record, "/")
+  end
+
+  test "it disallows a check if the rule indicates it is not allowed" do
+    rule = %Rules{user_agent: "*", allow: [], disallow: ["/"]}
+    warden_record = %Warden{rule: rule}
+
+    refute Warden.permitted?(warden_record, "/")
+  end
+
+  test "it indicates if the outcome is ambiguous" do
+    rule = %Rules{user_agent: "*", allow: ["/"], disallow: ["/"]}
+    warden_record = %Warden{rule: rule}
+
+    assert :ambiguous = Warden.permitted?(warden_record, "/")
+  end
+end
